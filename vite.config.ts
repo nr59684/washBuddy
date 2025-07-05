121a0f7e0340588b7db88e43a8d16f5ca0081760
Nilesh Parshotam Rijhwani <54324654+nr59684@users.noreply.github.com>
2025-07-06 01:53:51 +0200

    PWA adaptability

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path';
 import { readFileSync, writeFileSync } from 'fs';
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
@@ -15,6 +16,37 @@ export default defineConfig(({ mode }) => {
     plugins: [
       react(),
       tailwindcss(),
+      VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Wash Buddy - Dorm Laundry Tracker',
+        short_name: 'Wash Buddy',
+        description: 'A Progressive Web App to track and manage the status of washing machines in a dorm.',
+        theme_color: '#0284c7',
+        background_color: '#f1f5f9',
+        display: 'standalone',
+        scope: '.',
+        start_url: '.',
+        icons: [
+          {
+            src: 'https://i.imgur.com/O9N4p5p.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'https://i.imgur.com/O9N4p5p.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'https://i.imgur.com/O9N4p5p.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    }),
       {
         name: 'replace-env-in-sw',
         // This hook runs after the entire bundle has been generated.
